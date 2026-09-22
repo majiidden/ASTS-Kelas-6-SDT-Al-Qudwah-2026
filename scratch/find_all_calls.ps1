04c1c01faa83d60750fd2ce234428d083888fb20
sdtaqlebak <mkalmajiid567@gmail.com>
2026-09-22 22:22:41 +0700

    Delete scratch directory
    hapus

--- scratch/find_all_calls.ps1
+++
@@ -1,8 +0,0 @@
-$lines = Get-Content 'index.html'
-for ($i = 14800; $i -lt 15000; $i++) {
-    if ($lines[$i] -match 'google\.script\.run') {
-        for ($j = $i; $j -lt $i + 60; $j++) {
-            Write-Host "$($j+1): $($lines[$j])"
-        }
-    }
-}
--- scratch/inspect_discussion.ps1
+++
@@ -1,7 +0,0 @@
-$text = [System.IO.File]::ReadAllText("index.html")
-$idx = $text.IndexOf("function openStudentDiscussion")
-if ($idx -ge 0) {
-    Write-Host $text.Substring($idx, [Math]::Min(1500, $text.Length - $idx))
-} else {
-    Write-Host "Not found"
-}
